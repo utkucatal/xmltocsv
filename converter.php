9d90a1bde7f752ae33fdd59b42211c0223697589
utkucatal <62041985+utkucatal@users.noreply.github.com>
2022-04-26 16:47:43 +0300

    Add files via upload

---
+++ converter.php
@@ -0,0 +1,46 @@
+<?php
+
+class converter
+{
+    public $success;
+    function create_csv($xml_file_input, $csv_file_output)
+    {
+        $csv_data = "";
+        if (file_exists($xml_file_input)) {
+            if (!$xml = simplexml_load_file($xml_file_input)) {
+                return $this->success = false;
+            }
+        } else {
+            return $this->success = false;
+        }
+
+        $output_file = fopen($csv_file_output, 'w');
+        $header = false;
+        foreach ($xml as $key => $value) {
+            if (!$header) {
+                if (!fputcsv($output_file, array_keys(get_object_vars($value)))) {
+                    return $this->success = false;
+                }
+                $header = true;
+            }
+            if (!fputcsv($output_file, get_object_vars($value))) {
+                return $this->success = false;
+            }
+        }
+        fclose($output_file);
+        return $this->success = true;
+    }
+}
+
+$converter = new Converter();
+$converter->create_csv('sample.xml', 'sample.csv');
+
+try {
+    if ($converter->success) {
+        echo 'The XML file has been converted to CSV';
+    } else {
+        throw new Exception("was_not_converted");
+    }
+} catch (Exception $e) {
+    echo "The XML file was not converted to CSV -> " . $e->getMessage();
+}
