6cb99c84290cefa2121893d67533f9beb8a4b22f
utkucatal <62041985+utkucatal@users.noreply.github.com>
2022-04-26 18:08:03 +0300

    Update converter.php

--- converter.php
+++ converter.php
@@ -2,16 +2,16 @@
 
 class converter
 {
-    public $success;
+    public $is_success;
     function create_csv($xml_file_input, $csv_file_output)
     {
         $csv_data = "";
         if (file_exists($xml_file_input)) {
             if (!$xml = simplexml_load_file($xml_file_input)) {
-                return $this->success = false;
+                return $this->is_success = false;
             }
         } else {
-            return $this->success = false;
+            return $this->is_success = false;
         }
 
         $output_file = fopen($csv_file_output, 'w');
@@ -19,16 +19,16 @@ class converter
         foreach ($xml as $key => $value) {
             if (!$header) {
                 if (!fputcsv($output_file, array_keys(get_object_vars($value)))) {
-                    return $this->success = false;
+                    return $this->is_success = false;
                 }
                 $header = true;
             }
             if (!fputcsv($output_file, get_object_vars($value))) {
-                return $this->success = false;
+                return $this->is_success = false;
             }
         }
         fclose($output_file);
-        return $this->success = true;
+        return $this->is_success = true;
     }
 }
 
@@ -36,7 +36,7 @@ $converter = new Converter();
 $converter->create_csv('sample.xml', 'sample.csv');
 
 try {
-    if ($converter->success) {
+    if ($converter->is_success) {
         echo 'The XML file has been converted to CSV';
     } else {
         throw new Exception("was_not_converted");
